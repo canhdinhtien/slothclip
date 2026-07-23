deb247720dc7de65d3c5e2daf0e918f10350d1b9
canhdinhtien <canhva20047@gmail.com>
2026-07-23 11:05:07 +0700

    feat: implement utils for data

---
+++ SPC/utils_data.py
@@ -0,0 +1,110 @@
+import torch
+import random
+
+from os.path import join as ospj
+from munch import Munch as mch
+from PIL import ImageFilter, ImageOps
+
+NORM_MEAN = (0.485, 0.456, 0.406)
+NORM_STD = (0.229, 0.224, 0.225)
+CLIP_NORM_MEAN = (0.48145466, 0.4578275, 0.40821073)
+CLIP_NORM_STD = (0.26862954, 0.26130258, 0.27577711)
+
+class UnNormalize(object):
+    def __init__(self, 
+                 mean=[0.485, 0.456, 0.406], 
+                 std=[0.229, 0.224, 0.225]):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        unnormed_tensor = torch.zeros_like(tensor)
+        for i, (t, m, s) in enumerate(zip(tensor, self.mean, self.std)):
+            unnormed_tensor[i] = t.mul(s).add(m)
+        return unnormed_tensor
+
+unnorm = UnNormalize()
+
+
+def configure_metadata(metadata_root):
+    metadata = mch()
+    metadata.image_ids = ospj(metadata_root, 'image_ids.txt')
+    metadata.image_ids_proxy = ospj(metadata_root, 'image_ids_proxy.txt')
+    metadata.class_labels = ospj(metadata_root, 'class_labels.txt')
+    return metadata
+
+
+def get_image_ids(metadata, proxy=False):
+    """
+    image_ids.txt has the structure
+
+    <path>
+    path/to/image1.jpg
+    path/to/image2.jpg
+    path/to/image3.jpg
+    ...
+    """
+    image_ids = []
+    suffix = '_proxy' if proxy else ''
+    with open(metadata['image_ids' + suffix]) as f:
+        for line in f.readlines():
+            image_ids.append(line.strip('\n'))
+    return image_ids
+
+
+def get_class_labels(metadata):
+    """
+    class_labels.txt has the structure
+
+    <path>,<integer_class_label>
+    path/to/image1.jpg,0
+    path/to/image2.jpg,1
+    path/to/image3.jpg,1
+    ...
+    """
+    class_labels = {}
+    with open(metadata.class_labels) as f:
+        for line in f.readlines():
+            image_id, class_label_string = line.strip('\n').split(',')
+            class_labels[image_id] = int(class_label_string)
+    return class_labels
+
+class GaussianBlur(object):
+    def __init__(self, p=0.5, radius_min=0.1, radius_max=2.0):
+        self.p = p
+        self.radius_min = radius_min
+        self.radius_max = radius_max
+
+    def __repr__(self):
+        return "{}(p={}, radius_min={}, radius_max={})".format(
+            self.__class__.__name__, self.p, self.radius_min, self.radius_max
+        )
+
+    def __call__(self, img):
+        if random.random() <= self.p:
+            radius = random.uniform(self.radius_min, self.radius_max)
+            return img.filter(ImageFilter.GaussianBlur(radius=radius))
+        else:
+            return img
+
+class Solarization(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __repr__(self):
+        return "{}(p={})".format(self.__class__.__name__, self.p)
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
+
+
+
